8280b700dc2d66e1d033fe2acdc59e08dfc193c7
Thiago Vinícius Freire de Araújo Ribeiro <thiagovfar@gmail.com>
2012-10-27 13:47:55 -0300

    Setup redirection url on production environment
    Same thing we did before with the default page: Every redirection should
    point to only one of the available domains.

--- UrlMaker/src/com/thiagovinicius/web/urlmkr/shared/DefaultValues.java
+++ UrlMaker/src/com/thiagovinicius/web/urlmkr/shared/DefaultValues.java
@@ -6,5 +6,6 @@ public class DefaultValues {
 
 	public static String DEV_MODE_TAG = "gae-devmode";
 	public static String MAIN_DOMAIN = "create.urlmkr.com";
+	public static String REDIRECT_DOMAIN = "urlmkr.com";
 
 }
