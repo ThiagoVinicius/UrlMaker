e8d021ed25bf2cfdac719663a2a61057619821a5
Thiago Vinícius Freire de Araújo Ribeiro <thiagovfar@gmail.com>
2012-10-27 19:42:47 -0300

    Screw JSP for now, doing redirect on GWT

--- UrlMaker/src/com/thiagovinicius/web/urlmkr/client/UrlMaker.java
+++ UrlMaker/src/com/thiagovinicius/web/urlmkr/client/UrlMaker.java
@@ -6,6 +6,7 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.place.shared.PlaceController;
 import com.google.gwt.place.shared.PlaceHistoryHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
@@ -13,6 +14,7 @@ import com.google.web.bindery.event.shared.EventBus;
 import com.thiagovinicius.web.urlmkr.client.mvp.AppActivityMapper;
 import com.thiagovinicius.web.urlmkr.client.mvp.AppPlaceHistoryMapper;
 import com.thiagovinicius.web.urlmkr.client.place.CreateRedirectPlace;
+import com.thiagovinicius.web.urlmkr.shared.DefaultValues;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -23,6 +25,10 @@ public class UrlMaker implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
+		if (redirectToMainDomain()) {
+			return; //don't let the app load, we're done.
+		}
+
 		ClientContext clientFactory = GWT.create(ClientContext.class);
 
 		pingServer(clientFactory);
@@ -43,6 +49,19 @@ public class UrlMaker implements EntryPoint {
 		RootPanel.get().add(appWidget);
 		// Goes to place represented on URL or default place
 		historyHandler.handleCurrentHistory();
+	}
+
+	private boolean redirectToMainDomain() {
+		if (GWT.isProdMode() && !Window.Location.getHost().startsWith("127.")) {
+			String host = Window.Location.getHost();
+			if (!host.equals(DefaultValues.MAIN_DOMAIN)) {
+				String url = Window.Location.getHref();
+				url = url.replaceFirst(host, DefaultValues.MAIN_DOMAIN);
+				Window.Location.replace(url);
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private void pingServer(ClientContext clientFactory) {
